2b8f3f57f342f80689ea376fb616ed539d2b8c10
Luchettinni <LuchettinniDev@outlook.com>
2019-08-27 11:49:14 -0300

    tuvieja

---
+++ Aranda.Luciano/Clase_03/Persona.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_03
+{
+    class Persona
+    {
+        public string nombre;
+        public string apellido;
+        public int dni;
+
+        public string Mostrar()
+        {
+
+            return nombre + " " + apellido + " " + dni.ToString();
+        }
+
+        public Persona(string nombre, string apellido, int dni)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.dni = dni;
+        }
+    }
+}
